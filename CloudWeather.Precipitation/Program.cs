41078258ac971848b77011a7e880f4cc292d53a9
Pedro Bruno R. Lourenço <68498467+pedrobrunolourenco@users.noreply.github.com>
2024-02-02 10:05:06 -0300

    COMMIT-001-INSTALANDO-DEPENDENCIAS

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,6 +1,14 @@
+
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.MapGet("observation/{zip}", (string zip, [FromQuery] int? days) => { 
+    return Results.Ok(zip);
+});
+
 
 app.Run();
 
