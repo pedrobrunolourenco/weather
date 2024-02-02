713ab75b85c2bd624373f70759404fd4874dee2a
Pedro Bruno R. Lourenço <68498467+pedrobrunolourenco@users.noreply.github.com>
2024-02-02 14:41:45 -0300

    COMMIT-BANCO-002-CRIADO

---
+++ CloudWeather.Report/DataAccess/WeatherReportDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Report.DataAccess
+{
+    public class WeatherReportDbContext : DbContext
+    {
+        public WeatherReportDbContext() { }
+
+
+        public WeatherReportDbContext(DbContextOptions<WeatherReportDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<WeatherReport> WeatherReport { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SnakeCaseIdentiTyTableNames(modelBuilder);
+        }
+
+        private static void SnakeCaseIdentiTyTableNames(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WeatherReport>(b => b.ToTable("weather_report"));
+        }
+
+    }
+}
---
+++ CloudWeather.Temperature/DataAccess/TemperatureDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Temperature.DataAccess
+{
+    public class TemperatureDbContext : DbContext
+    {
+
+        public TemperatureDbContext() { }
+
+
+        public TemperatureDbContext(DbContextOptions<TemperatureDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Temperature> Temperature { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SnakeCaseIdentiTyTableNames(modelBuilder);
+        }
+
+        private static void SnakeCaseIdentiTyTableNames(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Temperature>(b => b.ToTable("temperature"));
+        }
+
+    }
+
+
+}
+
